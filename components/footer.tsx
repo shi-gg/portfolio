20b56f3b12c0b6ce80bb7ed07f9813952f2d6107
Drijon <87547203+drijonh@users.noreply.github.com>
2026-01-03 14:31:54 +0100

    Change Github username (#11)

--- components/footer.tsx
+++ components/footer.tsx
@@ -53,7 +53,7 @@ function Info() {
                     Made with love & autism by
                     {/* Do not alter the href, image src and author name */}
                     <Link
-                        href="https://github.com/Luna-devv/portfolio"
+                        href="https://github.com/shi-gg/portfolio"
                         target="_blank"
                     >
                         <Badge
@@ -68,7 +68,7 @@ function Info() {
                                 height={18}
                                 className="rounded-full relative right-1.5 px-[1px]"
                             />
-                            Luna-devv
+                            Shi-gg
                         </Badge>
                     </Link>
                 </span>
